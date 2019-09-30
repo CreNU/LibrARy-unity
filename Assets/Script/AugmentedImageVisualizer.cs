5f9947fe81b59f68226f1fc013e9e189211da167
garbi23 <garbi23@naver.com>
2019-09-30 14:20:29 +0900

    Unity ARcore Upload

---
+++ Assets/Script/AugmentedImageVisualizer.cs
@@ -0,0 +1,64 @@
+//-----------------------------------------------------------------------
+// <copyright file="AugmentedImageVisualizer.cs" company="Google">
+//
+// Copyright 2018 Google Inc. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+// </copyright>
+//-----------------------------------------------------------------------
+
+namespace GoogleARCore.Examples.AugmentedImage
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Runtime.InteropServices;
+    using GoogleARCore;
+    using GoogleARCoreInternal;
+    using UnityEngine;
+
+    /// <summary>
+    /// Uses 4 frame corner objects to visualize an AugmentedImage.
+    /// </summary>
+    public class AugmentedImageVisualizer : MonoBehaviour
+    {
+        /// <summary>
+        /// The AugmentedImage to visualize.
+        /// </summary>
+        public AugmentedImage Image;
+        public GameObject Center;
+
+        /// <summary>
+        /// A model for the lower left corner of the frame to place when an image is detected.
+        /// </summary>
+        public GameObject Object1;
+        private float Maxdistance = 1.000f;
+
+        public void Update()
+        {
+            Center.transform.position = Camera.current.transform.position;
+
+            if (Image == null || Image.TrackingState != TrackingState.Tracking)
+            {
+                Object1.SetActive(false);
+                return;
+            }
+
+
+                Object1.SetActive(true);
+
+
+            
+        }
+    }
+}
