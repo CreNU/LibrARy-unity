f84fc9693e6523b3c4381b00020b01e8ed6c2387
GSpy-cpp <55821668+GSpy-cpp@users.noreply.github.com>
2019-10-02 18:34:13 +0900

    TargetObj 소스 추가
    DB에서 받아온 데이터를 바탕으로 책 위치를 계산함.

---
+++ Assets/Script/TargetObj.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    public const float BlockHeight = 0.45f;
+    public const float BlockWidth = 0.908f;//요건 y성분
+    public const float BlockDepth = 0.143f;//요건 x성분
+    public const float pivotx = 0;
+    public const float pivoty = -0.464f;
+    public const float pivotz = -1.44f;
+    public const int left = 1;
+    public const int right = 2;
+    //지금까지 상수였구연
+
+    public GameObject TargetObj;
+    public Text TextRow, TextCol, TextDir;//DB에서 받아온 값
+    public int IntRow, IntCol, IntDir;
+    public float x, y, z;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (TextRow == null || TextCol == null || TextDir == null)
+        {
+            //오류처리 추가바람 
+        }
+        //Text데이터들을 Int로 바꾸는 과정이 필요함 (TextRow, TextCol, TextDir) -> (IntRow, IntCol, IntDir)
+
+        //
+        if (IntDir == left)
+        {
+            x = pivotx - BlockDepth;
+        }
+        else
+        {
+            x = pivotx + BlockDepth;
+        }
+        y = pivoty + (IntCol - 1) * BlockWidth;
+        z = pivotz + (IntRow - 1) * BlockHeight;
+        TargetObj.transform.position = new Vector3(x, y, z);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        TargetObj.SetActive(true);
+    }
+}
