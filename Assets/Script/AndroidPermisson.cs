9a67b587014f5344b5034acaeb324440c387b361
garbi23 <garbi23@naver.com>
2019-10-03 19:04:28 +0900

    Tarfet Api 8,1수정

--- Assets/Script/AndroidPermisson.cs
+++ Assets/Script/AndroidPermisson.cs
@@ -1,21 +1,29 @@
 ﻿using UnityEngine;
 #if PLATFORM_ANDROID
 using UnityEngine.Android;
+using UnityEngine.UI;
 #endif
 
 
 public class AndroidPermisson : MonoBehaviour
 {
     GameObject dialog = null;
+    public Text Ptext;
 
     void Start()
     {
+        Debug.Log("퍼미션체크");
+        Ptext.text = "퍼미션체크1";
 #if PLATFORM_ANDROID
         if (!Permission.HasUserAuthorizedPermission("android.permission.INTERNET"))
         {
+            Debug.Log("퍼미션체크");
+            Ptext.text = "퍼미션체크1";
             Permission.RequestUserPermission("android.permission.INTERNET");
             dialog = new GameObject();
         }
+        Debug.Log("퍼미션체크");
+        Ptext.text = "퍼미션체크1";
 #endif
     }
 
@@ -24,6 +32,7 @@ public class AndroidPermisson : MonoBehaviour
 #if PLATFORM_ANDROID
         if (!Permission.HasUserAuthorizedPermission("android.permission.INTERNET"))
         {
+            Ptext.text = "퍼미션체크2";
             // The user denied permission to use the microphone.
             // Display a message explaining why you need it with Yes/No buttons.
             // If the user says yes then present the request again
