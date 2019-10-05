a1da5c5e52b62f286758bed419fcea8c7060a314
GSpy-cpp <55821668+GSpy-cpp@users.noreply.github.com>
2019-10-05 23:08:11 +0900

    Update BooksAR.cs

--- Assets/Script/BooksAR.cs
+++ Assets/Script/BooksAR.cs
@@ -7,48 +7,51 @@ using System.Text.RegularExpressions;
 
 public class BooksAR : MonoBehaviour
 {
-
+    Except Baseball;
     public Text title;
-    public GameObject popUP;
+    public GameObject PopUp;
     public static int SelectNum;
     string ObName;
-    string withoutNumbers;
+    string WithoutNumbers;
     int nTmp;
 
     public void Update()
     {
-        if (popUP.activeSelf == false && BtnItemClick.ClickStat == 1)
+
+        if (PopUp.activeSelf == true || BtneItemClick.ClickStat != 1 || EventSystem.current.currentSelectedGameObject == false)
         {
-            if (EventSystem.current.currentSelectedGameObject)
-            {
-                ObName = EventSystem.current.currentSelectedGameObject.name;
-                string strTmp = Regex.Replace(ObName, @"\D", "");
-                withoutNumbers = Regex.Replace(ObName, "[0-9]", "");
-                int.TryParse(strTmp, out nTmp);
-                Debug.Log("순서 인덱스 + " + nTmp);
-                SelectNum = nTmp;
-                GameObject BackPanel = popUP.transform.GetChild(0).gameObject;
-                GameObject ArOn = BackPanel.transform.GetChild(2).gameObject;
-                GameObject ArOff = BackPanel.transform.GetChild(3).gameObject;
-
-                if (withoutNumbers == "(Clone)")
-                {
-                    title.text = GetBookInfo.BooksTitle[nTmp].ToString();
-
-                    popUP.SetActive(true);
-                    if (!GetBookInfo.BooksAR[nTmp])
-                    {
-                        ArOn.SetActive(false);
-                        ArOff.SetActive(true);
-                    }
-                    else
-                    {
-                        ArOn.SetActive(true);
-                        ArOff.SetActive(false);
-                    }
-                    BtnItemClick.ClickStat = 0;
-                }
-            }
+            return;
         }
+
+        ObName = EventSystem.current.currentSelectedGameObject.name;
+        string strTmp = Regex.Replace(ObName, @"\D", "");
+        WithoutNumbers = Regex.Replace(ObName, "[0-9]", "");
+        int.TryParse(strTmp, out nTmp);
+        Debug.Log("순서 인덱스 + " + nTmp);
+        SelectNum = nTmp;
+        GameObject BackPanel = PopUp.transform.GetChild(0).gameObject;
+        GameObject ArOn = BackPanel.transform.GetChild(2).gameObject;
+        GameObject ArOff = BackPanel.transform.GetChild(3).gameObject;
+        
+
+        if (WithoutNumbers != "(Clone)")
+        {
+            return;
+        }
+        
+        title.text = GetBookInfo.BooksTitle[nTmp].ToString();
+        PopUp.SetActive(true);
+
+        if (GetBookInfo.BookAR[nTemp] == NULL)
+        {
+            ArOn.SetActive(false);
+            ArOff.SetActive(true);
+        }
+        else
+        {
+            ArOn.SetActive(true);
+            ArOff.SetActive(false);
+        }
+        BtnItemClick.ClickStat = 0;
     }
 }
